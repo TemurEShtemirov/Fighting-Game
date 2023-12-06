c22e8ccd4bde6e362e1b246e136218ff89663a8c
TemurEShtemirov <temureshtemirov10@gmail.com>
2023-12-06 15:40:46 +0500

    adding player and enemy

--- main.js
+++ main.js
@@ -2,14 +2,31 @@ const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
 canvas.width = 1024;
-canvas.height = 576;         
+canvas.height = 576;
 
-c.fillRect(0, 0, canvas.width,canvas.height);
+c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
-    constructor (position) {
-        this.position = position 
-    }
+  constructor(position) {
+    this.position = position;
+  }
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 50, 150);
+  }
 }
 
-const player = new Sprite()
+const player = new Sprite({
+  x: 0,
+  y: 0,
+});
+player.draw();
+
+console.log(player);
+
+const enemy = new Sprite({
+  x: 400,
+  y: 100,
+});
+
+enemy.draw();
