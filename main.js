d3de8a08004c77f23753d81acaa8e8de0169c203
TemurEShtemirov <temureshtemirov10@gmail.com>
2023-12-06 15:52:17 +0500

    adding animation

--- main.js
+++ main.js
@@ -7,26 +7,60 @@ canvas.height = 576;
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
-  constructor(position) {
+  constructor(position, velocity) {
     this.position = position;
+    this.velocity = velocity;
   }
   draw() {
     c.fillStyle = "red";
     c.fillRect(this.position.x, this.position.y, 50, 150);
   }
+  update() {
+    this.draw();
+    this.position.y += 10;
+  }
 }
 
-const player = new Sprite({
-  x: 0,
-  y: 0,
-});
+const player = new Sprite(
+  {
+    x: 0,
+    y: 0,
+  },
+  {
+    x: 0,
+    y: 0,
+  }
+);
 player.draw();
 
-console.log(player);
-
-const enemy = new Sprite({
-  x: 400,
-  y: 100,
-});
+const enemy = new Sprite(
+  {
+    x: 400,
+    y: 100,
+  },
+  {
+    x: 0,
+    y: 0,
+  }
+);
 
 enemy.draw();
+
+function animate() {
+  window.requestAnimationFrame(animate);
+
+  // Clear the canvas
+  c.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Update and draw the player
+  player.update();
+  //   player.draw();
+
+  // Update and draw the enemy
+  enemy.update();
+  //   enemy.draw();
+}
+animate();
+
+console.log(player);
+console.log(enemy);
